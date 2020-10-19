537d8b2dabc43142f430043816f3d34a484dd31f
Fouad J <fouad.j@live.fr>
2020-10-19 16:36:41 +0200

    factor logic

--- src/main/java/com/jfouad/tennis/TennisGame.java
+++ src/main/java/com/jfouad/tennis/TennisGame.java
@@ -1,10 +1,29 @@
 package com.jfouad.tennis;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
 
 import static com.jfouad.tennis.Score.getScoreLabel;
 
 public class TennisGame {
+    private final BiPredicate<Player, Player> isDeuceScore = (playerOne, playerTwo) -> playerOne.getScore() >= 3 && playerOne.getScore() == playerTwo.getScore();
+    private final BiPredicate<Player, Player> isEqualScore = (playerOne, playerTwo) -> playerOne.getScore() == playerTwo.getScore();
+    private final BiPredicate<Player, Player> isPlayerWins = (playerOne, playerTwo) ->
+            (playerOne.getScore() > 3 && playerOne.getScore() >= playerTwo.getScore() + 2) ||
+            (playerTwo.getScore() > 3 && playerTwo.getScore() >= playerOne.getScore() + 2);
+    private final BiPredicate<Player, Player> isPlayerHasAdvantage = (playerOne, playerTwo) ->
+            (playerOne.getScore() > 3 && playerOne.getScore() == playerTwo.getScore() + 1) ||
+            (playerTwo.getScore() > 3 && playerTwo.getScore() == playerOne.getScore() + 1);
+
+    private final Map<BiPredicate<Player, Player>, BiFunction<Player, Player, String>> rules =
+            new LinkedHashMap<BiPredicate<Player, Player>, BiFunction<Player, Player, String>>() {{
+                put(isDeuceScore, (playerOne, playerTwo) -> "Deuce");
+                put(isEqualScore, (playerOne, playerTwo) -> getScoreLabel(playerOne.getScore()) + " - " + getScoreLabel(playerTwo.getScore()));
+                put(isPlayerHasAdvantage, (playerOne, playerTwo) -> "Advantage " + getLeadGameName(playerOne, playerTwo));
+                put(isPlayerWins, (playerOne, playerTwo) -> getLeadGameName(playerOne, playerTwo) + " wins");
+            }};
 
     private Player playerOne;
     private Player playerTwo;
@@ -14,41 +33,13 @@ public class TennisGame {
         this.playerTwo = new Player(namePlayerTwo, 0);
     }
 
-    private final BiPredicate<Player, Player> isPlayerHasAdvantage = (playerOne, playerTwo) ->
-            (playerOne.getScore() > 3 && playerOne.getScore() == playerTwo.getScore() + 1) ||
-                    (playerTwo.getScore() > 3 && playerTwo.getScore() == playerOne.getScore() + 1);
-
-    private final BiPredicate<Player, Player> isDeuceScore = (playerOne, playerTwo) -> playerOne.getScore() >= 3 && playerOne.getScore() == playerTwo.getScore();
-    private final BiPredicate<Player, Player> isLoveScore = (playerOne, playerTwo) -> playerOne.getScore() == 0 && playerTwo.getScore() == 0;
-    private final BiPredicate<Player, Player> isEqualScore = (playerOne, playerTwo) -> playerOne.getScore() == playerTwo.getScore();
-    private final BiPredicate<Player, Player> isPlayerWins = (playerOne, playerTwo) ->
-            (playerOne.getScore() > 3 && playerOne.getScore() >= playerTwo.getScore() + 2) ||
-                    (playerTwo.getScore() > 3 && playerTwo.getScore() >= playerOne.getScore() + 2);
-
-    private final BiPredicate<Player, Player> isFirstPlayerLead = (playerOne, playerTwo) -> playerOne.getScore() > playerTwo.getScore();
-
     public String getScore() {
-        if (isLoveScore.test(playerOne, playerTwo)) {
-            return getScoreLabel(0) + " - " + getScoreLabel(0);
-        }
-
-        if (isDeuceScore.test(playerOne, playerTwo)) {
-            return "Deuce";
-        }
-
-        if (isEqualScore.test(playerOne, playerTwo)) {
-            return getScoreLabel(playerOne.getScore()) + " - " + getScoreLabel(playerTwo.getScore());
-        }
-
-        if (isPlayerHasAdvantage.test(playerOne, playerTwo)) {
-            return "Advantage " + getLeadGameName(playerOne, playerTwo);
-        }
-
-        if (isPlayerWins.test(playerOne, playerTwo)) {
-            return getLeadGameName(playerOne, playerTwo) + " wins";
-        }
-
-        throw new RuntimeException("Unknown scenario");
+        return rules.entrySet().stream()
+                .filter(e -> e.getKey().test(playerOne, playerTwo))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .map(e -> e.apply(playerOne, playerTwo))
+                .orElseThrow(() -> new RuntimeException("Unknown scenario")); // TODO create custom Exception
     }
 
     public void playerOneScores() {
@@ -60,7 +51,7 @@ public class TennisGame {
     }
 
     private String getLeadGameName(Player playerOne, Player playerTwo) {
-        return isFirstPlayerLead.test(playerOne, playerTwo)
+        return playerOne.getScore() > playerTwo.getScore()
                 ? playerOne.getName()
                 : playerTwo.getName();
     }
